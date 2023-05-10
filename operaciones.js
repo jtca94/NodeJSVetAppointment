87e1b59b95ab8135910676ed888427ed579645e5
jtcannobbio <tomas.cannobbio@gmail.com>
2023-05-09 20:13:52 -0400

    added chalk dependency for color in console

--- operaciones.js
+++ operaciones.js
@@ -1,7 +1,11 @@
 import {writeFile, readFile} from "fs/promises";
+import chalk from "chalk";
+
+//  sets the level of color support for vscode debugger console
+chalk.level = 1
 
 export async function write(newInfo, values) {
-    // recibe objeto con datos de la cita y array con argumentos para validar
+  // recibe objeto con datos de la cita y array con argumentos para validar
   try {
     // validacion de argumentos
     if (values.length != 5) {
@@ -17,9 +21,8 @@ export async function write(newInfo, values) {
     // escritura de datos previos + nuevos
     await writeFile("citas.json", JSON.stringify(newData));
     console.log("Cita registrada");
-  } 
-  // manejo de errores por consola
-  catch (error) {
+  } catch (error) {
+    // manejo de errores por consola
     throw new Error("Error writing to citas.json: " + error.message);
   }
 }
@@ -27,14 +30,34 @@ export async function write(newInfo, values) {
 export async function read() {
   try {
     const data = await readFile("citas.json", "utf-8");
-    // validacion de citas registradas 
+    // validacion de citas registradas
     if (data.length == 2) {
       throw new Error("No hay citas registradas");
     }
+    const array = JSON.parse(data);
+    
     // impresion de citas registradas
-    console.log("Las citas registradas son: \n", JSON.parse(data));
-  } 
-  catch (error) {
+    array.forEach((element) => {
+      console.log(
+        chalk.cyan("Nombre: ") +
+          element.nombre +
+          "\n" +
+          chalk.cyan("Edad: ") +
+          element.edad +
+          "\n" +
+          chalk.cyan("Animal: ") +
+          element.animal +
+          "\n" +
+          chalk.cyan("Color: ") +
+          element.color +
+          "\n" +
+          chalk.cyan("Enfermedad: ") +
+          element.enfermedad +
+          "\n" +
+          "---------------------------------"
+      );
+    });
+  } catch (error) {
     console.log(error);
   }
 }
